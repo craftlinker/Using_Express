95ef45eda3d9c28c6b3c774e53f33939fa04e564
craftlinker <37788621+craftlinker@users.noreply.github.com>
2018-04-14 09:43:03 -0700

    Update index.js
    uncommit out

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ app.set('view engine', 'handlebars');
 
 app.set("port", process.env.PORT || 3000);
 
-// app.get("/beginning", (request, response) => {
-//     response.render('beginning');
-// });
+app.get("/beginning", (request, response) => {
+     response.render('beginning');
+});
 
 app.get("/", (request, response) => {
     response.render('beginning');
 });
 
-// app.get("/middle", (request, response) => {
-//     response.render('middle');
-// });
+ app.get("/middle", (request, response) => {
+     response.render('middle');
+ });
 
-// app.get("/end", (request, response) => {
-//     response.render('end');
-// });
+app.get("/end", (request, response) => {
+     response.render('end');
+ });
 
 app.use((request, response) => {
     response.status(404);
